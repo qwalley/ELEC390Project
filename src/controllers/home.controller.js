74e5bd277aeca75e5fea52f089c4fcb4d9729b38
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2017-04-03 19:41:39 -0400

    test after pull

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -19,15 +19,14 @@ module.exports = function (req, res) {
 	// log errors to console
 	URLScraper.stderr.on('data', (data) => {
 		result = data.toString();
-		consonle.log(result);
+		console.log(result);
 	});
 
 	URLScraper.on('close', (code) => {
 		var titles = {
 			title1: '',
 			title2: '',
-			title3: '',
-			title4: ''
+			title3: ''
 		};
 		const title1Scraper = spawn('python', ["webscraper.py", 'title', URLs[0]]);
 		var result1
